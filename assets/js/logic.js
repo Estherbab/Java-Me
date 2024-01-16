81d087b029dd3b3d26141e1ede94d83249a2baa7
Estherbab <153003348+Estherbab@users.noreply.github.com>
2024-01-16 06:19:24 +0000

    created questions in js and choices btns in html

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,11 +5,12 @@ var timeEl = document.querySelector("#time")
 var startScreen = document.querySelector(".start")
 var startButton = document.querySelector(".btn")
 var displayquestions = document.querySelector(".hide")
+var questionsContainer = document.getElementById("#questions");
 
 
 
-// The timer variable is declared above and this sets the timer to start. The timer shows //
-// on the window and is also shown in the console log. Once the timer gets to 0 it stops //
+// The timer variable is declared above and this sets the timer to start. The timer shows
+// on the window and is also shown in the console log. Once the timer gets to 0 it stops
 var timerInterval = setInterval(function () {
     if (secondscounter -= 1) {
         timer.textContent = secondscounter;
@@ -18,8 +19,9 @@ var timerInterval = setInterval(function () {
         clearInterval(timerInterval);
         sendMessage();
     }
-    }, 1000); //1 second = 1000 milliseconds //
+    }, 1000); //1 second = 1000 milliseconds
 
+// Function declared when start button is clicked, the startscreen is hidden and quiz screen shown    
 function startQuiz() {
     console.log("Quiz Started!");
     startScreen.style.display = 'none';
@@ -28,3 +30,19 @@ function startQuiz() {
     start.classList.add('hide');
 }
 startButton.addEventListener('click', startQuiz);
+
+// Function to display the codequizQuestions once the start button is clicked
+function displayquestions() {
+    console.log(codequizQuestions)
+eachQuestion.textContent = codequizQuestions.questions;
+answerChoices.innerHTML = '';
+codequizQuestions.answers.forEach ((answers) => {
+const choiceButton = document. createElement ( 'button');
+choiceButton.textContent = answers;
+choiceButton.classList.add('choice');
+choiceButton.addEventListener ('click', () => checkAnswer (index)); 
+answerChoices.appendChild(choiceButton);
+});
+}
+
+nextButton.style.display = 'none';
---
+++ assets/js/questions.js
@@ -0,0 +1,43 @@
+
+var codequizQuestions = [
+    { 
+      question:'How many ways are there to display a variable in javascript?',
+      choices: {
+        a: "five", 
+        b: "four", 
+        c: "three", 
+        d: "two"
+      },  
+      correctanswer: 'five'
+     },
+     { 
+      question:'There are 4 ways to display data in javascript, which one is incorrect?',
+      choices: {
+        a: "innerHTML", 
+        b: "console.log", 
+        c: "window.alert", 
+        d: "inline-flex"
+      },  
+      correctanswer: 'inlinie-flex'
+     },
+     {
+      question:'How many javscript data types are there?',
+      choices: {
+        a: "five", 
+        b: "four", 
+        c: "three", 
+        d: "two"
+      },
+     correctanswer: 'five'
+     },
+     { 
+      question:'An array in JavaScript is a type of global object that is used to store?',
+      choices: {
+        a: "numbers", 
+        b: "strings", 
+        c: "objects", 
+        d: "all the above"
+      },  
+      correctanswer: 'all the above'
+     },
+ ]
