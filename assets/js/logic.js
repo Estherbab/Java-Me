809315247f592a07b86c4555542236aa35c36be4
Estherbab <153003348+Estherbab@users.noreply.github.com>
2024-01-16 03:51:10 +0000

    declared the var for the startscreen & button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,11 +2,15 @@ var timer = document.querySelector("#time")
 var secondscounter = 60
 var timeLeft = 10
 var timeEl = document.querySelector("#time")
+var startScreen = document.querySelector(".start")
+var startButton = document.querySelector(".btn")
 
 
 
 
-// This sets the time in variable ..........//
+
+// The timer variable is declared above and this sets the timer to start. The timer shows //
+// on the window and is also shown in the console log. Once the timer gets to 0 it stops //
 var timerInterval = setInterval(function () {
     if (secondscounter -= 1) {
         timer.textContent = secondscounter;
@@ -15,4 +19,4 @@ var timerInterval = setInterval(function () {
         clearInterval(timerInterval);
         sendMessage();
     }
-    }, 1000); //1 second = 1000 milliseconds.........//
+    }, 1000); //1 second = 1000 milliseconds //
