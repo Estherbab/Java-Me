2c6bc26679a0c5dfc8e40e0dd5417ac6af19235e
Estherbab <153003348+Estherbab@users.noreply.github.com>
2024-01-18 18:30:42 +0000

    added choice buttons and quiz questions page

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,55 +1,93 @@
-// Declaring all required variables
+// Declaring all the required variables for the code quiz
 var timer = document.querySelector(".timer")
 var secondscounter = 60
 var timeLeft = 10
 var timeEl = document.querySelector(".timer")
 var startScreen = document.querySelector(".start")
 var startButton = document.querySelector(".btn")
-var displayquestions = document.querySelector(".hide")
-var questionsContainer = document.getElementById("#questions");
-var endScreen = document.getElementById("#end-screen")
-var finalScore = document.getElementById("#final-score")
-var initials = document.getElementById("#initials")
-var submit = document.getElementById("#submit")
-var feedback = document.getElementById("#feedback")
+//var displayquestions = document.querySelector(".hide")
+var questionsContainer = document.getElementById("questions");
+var endScreen = document.getElementById("end-screen")
+var finalScore = document.getElementById("final-score")
+var initials = document.getElementById("initials")
+var submit = document.getElementById("submit")
+var feedback = document.getElementById("feedback")
 var highScores = document.querySelector(".scores")
+var questiontitle = document.getElementById("question-title")
+var choicesEl = document.getElementById("choices")
+var choiceButton = document.getElementById("choices")
 
 
 
 // The timer variable is declared above and this sets the timer to start. The timer shows
 // on the window and is also shown in the console log. Once the timer gets to 0 it stops
 var timerInterval = setInterval(function () {
-    if (secondscounter -= 1) {
-        timer.textContent = secondscounter;
-        console.log(secondscounter);
-    } else if (secondscounter === 0) {
-        clearInterval(timerInterval);
+    if (secondscounter -= 1) {                                    // timer logs every second
+        timer.textContent = secondscounter;                      // timer textcontent shows in window
+     //   console.log(secondscounter);                            // timer will appear in the console
+    } else if (secondscounter === 0) {                         // when counter is = 0 stop count
+        clearInterval(timerInterval);                         // clear the counter
         sendMessage();
     }
     }, 1000); //1 second = 1000 milliseconds
 
 // Function declared when start button is clicked, the startscreen is hidden and quiz screen shown    
 function startQuiz() {
-    console.log("Quiz Started!");
-    startScreen.style.display = 'none';
-    displayquestions.style.display = 'block';
-    console.log(displayquestions)
-    start.classList.add('hide');
+    console.log("Quiz Started!");                                     // console.log when quiz starts
+    startScreen.style.display = 'none';                              // hides the start screen
+    console.log(questionsContainer)                                  // displays the questions in console
+    questionsContainer.style.display = 'block';                       // displays the questions in window
+    start.classList.add('hide');                                  // hides the start button
+    displayquestions()
 }
 startButton.addEventListener('click', startQuiz);
 
+var questionIndex = 0
 // Function to display the codequizQuestions once the start button is clicked
 function displayquestions() {
-    console.log(codequizQuestions)
-eachQuestion.textContent = codequizQuestions.questions;
-answerChoices.innerHTML = '';
-codequizQuestions.answers.forEach ((answers) => {
-const choiceButton = document. createElement ( 'button');
-choiceButton.textContent = answers;
-choiceButton.classList.add('choice');
-choiceButton.addEventListener ('click', () => checkAnswer (index)); 
-answerChoices.appendChild(choiceButton);
+    choicesEl.innerHTML = ""; // done to
+    var currentQuestion = codequizQuestions[questionIndex];
+    console.log(currentQuestion)
+    console.log(questionIndex)
+    questiontitle.textContent = currentQuestion.question
+    for (let i = 0; i < currentQuestion.choices.length; i++) {
+        let choiceButton = document.createElement('button');
+        choiceButton.textContent = currentQuestion.choices[i]
+        choicesEl.appendChild(choiceButton)}
+
+// add event listener on choices buttons to
+choiceButton.addEventListener("click", function(event){
+    console.log(event.target.textContent)
+    if(event.target.textContent === currentQuestion.correctanswer) {
+        console.log("correct") // checking the string is correct
+        
+    } else {
+        console.log("incorrect");
+    }
+    
+    if(questionIndex < codequizQuestions.length - 1){
+        questionIndex++
+        displayquestions();
+    } else {
+        return;
+    }
 });
+
+//console.log("you clicked choice")
 }
 
-nextButton.style.display = 'none';
+
+//eachQuestion.textContent = codequizQuestions.questions;
+//choicesEl.innerHTML = '';
+//codequizQuestions.forEach((arrayitem) => {
+
+//var questionTitle = document.getElementById("question-title")
+//questionTitle.textContent = arrayitem.question
+//choiceButton.textContent = answers;
+//choiceButton.classList.add('choice');
+//choiceButton.addEventListener ('click', () => checkAnswer (index)); 
+//answerChoices.appendChild(choiceButton);
+//});
+//}
+
+//nextButton.style.display = 'none';
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,42 +2,22 @@
 var codequizQuestions = [
     { 
       question:'How many ways are there to display a variable in javascript?',
-      choices: {
-        a: "five", 
-        b: "four", 
-        c: "three", 
-        d: "two"
-      },  
+      choices: ["five", "four", "three", "two"],  
       correctanswer: 'five'
      },
      { 
       question:'There are 4 ways to display data in javascript, which one is incorrect?',
-      choices: {
-        a: "innerHTML", 
-        b: "console.log", 
-        c: "window.alert", 
-        d: "inline-flex"
-      },  
+      choices: ["innerHTML", "console.log", "window.alert", "inline-flex"],  
       correctanswer: 'inlinie-flex'
      },
      {
       question:'How many javscript data types are there?',
-      choices: {
-        a: "five", 
-        b: "four", 
-        c: "three", 
-        d: "two"
-      },
+      choices: ["five", "four", "three", "two"],
      correctanswer: 'five'
      },
      { 
       question:'An array in JavaScript is a type of global object that is used to store?',
-      choices: {
-        a: "numbers", 
-        b: "strings", 
-        c: "objects", 
-        d: "all the above"
-      },  
+      choices: ["numbers", "strings", "objects", "all the above"],  
       correctanswer: 'all the above'
      },
  ]
