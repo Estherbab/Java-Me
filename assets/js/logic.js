187f2e4c6d7878bf9625d15d321ad0f492cc3a8c
Estherbab <153003348+Estherbab@users.noreply.github.com>
2024-01-20 18:28:16 +0000

    created submitForm function & submit eventlistener

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -9,8 +9,8 @@ var startButton = document.querySelector(".btn")
 var questionsContainer = document.getElementById("questions");
 var endScreen = document.getElementById("end-screen")
 var finalScore = document.getElementById("final-score")
-var initials = document.getElementById("initials")
-var submit = document.getElementById("submit")
+var inputInitials = document.getElementById("initials")
+var submitButton = document.getElementById("submit")
 var feedback = document.getElementById("feedback")
 var highScores = document.querySelector(".scores")
 var questiontitle = document.getElementById("question-title")
@@ -19,67 +19,92 @@ var choicesEl = document.getElementById("choices")
 
 // Function declared when start button is clicked, the startscreen is hidden and quiz screen shown    
 function startQuiz() {
-// The timer variable is declared above and this sets the timer to start. The timer shows
+// The timer variable is declared below and this sets the timer to start. The timer shows
 // on the window and is also shown in the console log. Once the timer gets to 0 it stops
 var timerInterval = setInterval(function () {
     if (secondscounter >= 1) { 
-        secondscounter --                                                       // -1 is deducted from the timer
-        timer.textContent = secondscounter;                                    // timer textcontent shows in window
-     //   console.log(secondscounter);                                        // timer will appear in the console
-    } else if (secondscounter <= 0) {                                        // if timer is less than or = to 0 stop the count
-        timer.textContent = secondscounter;                                 // the textcontent of the timer is equal to the seconds
-        clearInterval(timerInterval);                                      // stop & clear the timerinterval 
+        secondscounter --                                                                     // -1 is deducted from the timer
+        timer.textContent = secondscounter;                                                  // timer textcontent shows in window
+     //   console.log(secondscounter);                                                      // timer will appear in the console
+    } else if (secondscounter <= 0) {                                                      // if timer is less than or = to 0 stop the count
+        timer.textContent = secondscounter;                                               // the textcontent of the timer is equal to the seconds
+        clearInterval(timerInterval);                                                    // stop & clear the timerinterval 
        // sendMessage();
     }
     }, 1000); //1 second = 1000 milliseconds
 
-    console.log("Quiz Started!");                                         // console.log when quiz starts
-    startScreen.style.display = 'none';                                  // hides the start screen
-    console.log(questionsContainer)                                     // displays the questions in the console
-    questionsContainer.style.display = 'block';                        // displays the questions container in the window
-    start.classList.add('hide');                                      // hides the start button
+    console.log("Quiz Started!");                                                          // console.log when quiz starts
+    startScreen.style.display = 'none';                                                   // hides the start screen
+    console.log(questionsContainer)                                                      // displays the questions in the console
+    questionsContainer.style.display = 'block';                                         // displays the questions container in the window
+    start.classList.add('hide');                                                       // hides the start button
     displayquestions()
 }
-startButton.addEventListener('click', startQuiz);
+startButton.addEventListener('click', startQuiz);                                           // once start button is clicked the quiz starts
 
 var questionIndex = 0
 // Function to display the codequizQuestions once the start button is clicked
 function displayquestions() {
-    choicesEl.innerHTML = "";                                            // done to retrieve whats inside the choices element in html
-    var currentQuestion = codequizQuestions[questionIndex];             // variable for codequizquestions array with the index for the question title
-    console.log(currentQuestion)                                       // to see the currentquestion in the console
-    console.log(questionIndex)                                        // to see the question number in console
-    questiontitle.textContent = currentQuestion.question             // this is to display the textcontent of each question
-    for (let i = 0; i < currentQuestion.choices.length; i++) {      // 
-        let choiceButton = document.createElement('button');       // to create the choice button for each question
-        choiceButton.textContent = currentQuestion.choices[i]     // the text content for each choice button = the current question title, all the choices & the index number of the choices
-        choicesEl.appendChild(choiceButton)                      // adds a new child node to the existing parent node in html 
+    choicesEl.innerHTML = "";                                                                       // done to retrieve whats inside the choices element in html
+    var currentQuestion = codequizQuestions[questionIndex];                                        // variable for codequizquestions array with the index for the question title
+    console.log(currentQuestion)                                                                  // to see the currentquestion in the console
+    console.log(questionIndex)                                                                   // to see the question number in console
+    questiontitle.textContent = currentQuestion.question                                        // this is to display the textcontent of each question
+    for (let i = 0; i < currentQuestion.choices.length; i++) {                                 // set index variabe to 0, if index is less than the length of the questions in array then add 1 to loop to next question
+        let choiceButton = document.createElement('button');                                  // to create the choice button for each question
+        choiceButton.textContent = currentQuestion.choices[i]                                // the text content for each choice button = the current question title, all the choices & the index number of the choices
+        choicesEl.appendChild(choiceButton)                                                 // adds a new child node to the existing parent node in html 
 
 // add event listener on choices buttons to
-choiceButton.addEventListener("click", function(event){                         // function waits for the choicebutton to be clicked(event) then responds to it
-    console.log(event.target.textContent)                                      // console log to see if eventlistener is working
-    if(event.target.textContent === currentQuestion.correctanswer) {          // if the button clicked is = to the correct answer, then the answer is correct
-        console.log("correct")                                               // checking the string is correct
+choiceButton.addEventListener("click", function(event){                                   // function waits for the choicebutton to be clicked(event) then responds to it
+    console.log(event.target.textContent)                                                // console log to see if eventlistener is working
+    if(event.target.textContent === currentQuestion.correctanswer) {                    // if the button clicked is = to the correct answer, then the answer is correct
+        console.log("correct")                                                         // checking the string is correct
         
     } else {
-       console.log("incorrect");                                          // consolelog incorrect if the button clicked is the wrong answer
-        secondscounter -= 5                                               // deduct 5 seconds from timer if user clicks wrong answer
+       console.log("incorrect");                                                     // consolelog incorrect if the button clicked is the wrong answer
+        secondscounter -= 5                                                         // deduct 5 seconds from timer if user clicks wrong answer
     }
     
-    if(questionIndex < codequizQuestions.length - 1){                   // if the question index is less than the codequizquestions length -1 then keep going through the questions
-        questionIndex++                                                // this is to loop through each question (++ adds 1)
-        displayquestions();                                           // keep displaying the questions as you go through the loop
-    } else {                                                         // else if the above conditions are not met, execute the code below
-            console.log('last question')                            // when the last question is answered log in console
-            endScreen.classList.remove("hide")                     // the hide class of the end screen is removed and the endscreen is displayed
-            questions.style.display = "none"                      // the questions screen is hidden
-            secondscounter = 0                                   // secondscounter is emptied to 0
-            timer.style.display = "none"                        // hides the timer when endscreen is displayed
+    if(questionIndex < codequizQuestions.length - 1){                            // if the question index is less than the codequizquestions length -1 then keep going through the questions
+        questionIndex++                                                         // this is to loop through each question (++ adds 1)
+        displayquestions();                                                    // keep displaying the questions as you go through the loop
+    } else {                                                                  // else if the above conditions are not met, execute the code below
+            console.log('last question')                                     // when the last question is answered log in console
+            endScreen.classList.remove("hide")                              // the hide class of the end screen is removed and the endscreen is displayed
+            questions.style.display = "none"                               // the questions screen is hidden
+            secondscounter = 0                                            // secondscounter is emptied to 0
+            timer.style.display = "none"                                 // hides the timer when endscreen is displayed
     }
        
 });
     }   
 }
+
+
+function submitForm () {
+    console.log('submitBtn clicked');
+  
+}
+
+submitButton.addEventListener('click', submitForm)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //eachQuestion.textContent = codequizQuestions.questions;
 //choicesEl.innerHTML = '';
